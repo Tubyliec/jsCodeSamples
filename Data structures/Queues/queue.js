7597b67f337f2f50065f5eb21fd100fdb1ec1cb7
tubyliec <antikruk@vivaldi.net>
2025-01-05 22:05:20 +0300

    feat: add queue

---
+++ Data structures/Queues/queue.js
@@ -0,0 +1,45 @@
+class Item {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = 0;
+    this.tail = 0;
+    this.length = 0;
+  }
+
+  getUnderlyingList() {
+    let list = null;
+    let counter = 0;
+    for (let key in this.items) {
+      list = { value: this.items[key], next: list };
+    }
+    return list;
+  }
+
+  enqueue(value) {
+    this.items[this.tail] = value;
+    this.tail += 1;
+  }
+
+  dequeue() {
+    const ITEM = this.items[this.head];
+    delete this.items[this.head];
+    this.head += 1;
+    return ITEM;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(3);
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(8);
+queue.enqueue(7);
+
+console.log(queue.getUnderlyingList()); // returns { value: 3, next: null }
